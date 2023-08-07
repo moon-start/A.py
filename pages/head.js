edde7facfbe27d8f484cb3499042e67559697114
moon-start <login0516mp4@gmail.com>
2023-08-08 01:42:39 +0800

    2023/08/08 週二  1:42:39.57

--- pages/head.js
+++ pages/head.js
@@ -1,15 +1,15 @@
-// 獲取當前 URL
-var currentURL = window.location.href;
-// 指定目標 URL
-// var targetURL = "http://localhost/moon-start/pages/index.html";
+// // 獲取當前 URL
+// var currentURL = window.location.href;
+// // 指定目標 URL
+// // var targetURL = "http://localhost/moon-start/pages/index.html";
 
         
-// 獲取檔名
-var fileName = currentURL.substring(currentURL.lastIndexOf('/') + 1);
-var targetURL = "http://localhost/moon-start/pages/"+fileName;
+// // 獲取檔名
+// var fileName = currentURL.substring(currentURL.lastIndexOf('/') + 1);
+// var targetURL = "http://localhost/moon-start/pages/"+fileName;
 
-// 檢查是否需要導向
-if (  window.location.href  !== targetURL) {
-    // 導向至目標 URL
-window.location.href = targetURL;
-}
+// // 檢查是否需要導向
+// if (  window.location.href  !== targetURL) {
+//     // 導向至目標 URL
+// window.location.href = targetURL;
+// }
